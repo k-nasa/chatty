3b36acf72a52b9d4b1d6a6c7abe2f0b3f4886846
k-nasa <htilcs1115@gmail.com>
2019-03-02 20:59:53 +0900

    雛形を修正

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {Header} from './components/header';
 
 const App = () => (
-  <div className="container">
+  <div className="container bg-light">
     <BrowserRouter>
       <div>
-        <Route exact path='/' component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/friends' component={Friends} />
+        <Header />
+        <Route exact path="/" component={Home} />
+        <Route path="/user/new" component={UserNew} />
+        <Route path="/sessions/new" component={SessionsNew} />
       </div>
     </BrowserRouter>
   </div>
-)
+);
 
 const Home = () => (
   <div>
     <h2>Home</h2>
-    <p>Welcome to ようこそ</p>
+    <p>Welcome to ようこそ ジャパリパーク</p>
   </div>
-)
-const About = () => (
+);
+const UserNew = () => (
   <div>
     <h2>About</h2>
     <p>フレンズに投票するページです</p>
   </div>
-)
-const Friends = () => (
+);
+const SessionsNew = () => (
   <div>
     <h2>Friends</h2>
     <p>ここにフレンズのリストを書きます</p>
   </div>
-)
+);
 
-export default App
+export default App;
