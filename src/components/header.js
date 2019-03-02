855dad10e273cb173e23fb1f9c1e4c6a944cd75a
k-nasa <htilcs1115@gmail.com>
2019-03-02 20:59:22 +0900

    feat: add header component

---
+++ src/components/header.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export const Header = () => (
+  <header>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Chatty
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <i class="fas fa-bars" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <Link className="nav-link" to="/users/new">
+              新規登録
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/sessions/new">
+              ログイン
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+);
